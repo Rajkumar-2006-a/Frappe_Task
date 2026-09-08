ae6471eca3b6293015a8d262843ee5f2106b8589
Rajkumar A . <rajkumar445912@gmail.com>
2026-09-08 14:09:36 +0530

    User fixture updated

--- library/task.py
+++
@@ -1,5 +0,0 @@
-import frappe
-def daily_maintenance():
-    frappe.log_error("Hello for the  task testing")
-    print("Executed the do something")
-    
--- library/tests/test.py
+++
@@ -1,4 +0,0 @@
-import frappe
-
-def before_tests():
-    print("✅ before_tests hook is working!")
